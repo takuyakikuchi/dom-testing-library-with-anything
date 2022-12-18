cecee1ab35025b8c3e6f2618797d17927a5695e0
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-12-18 19:08:14 +0900

    react-dom-test

--- react.test.js
+++ react.test.js
@@ -1,21 +1,46 @@
+// To add handy assertions to Jest
 import '@testing-library/jest-dom/extend-expect'
-import * as React from 'react'
-import userEvent from '@testing-library/user-event'
+import React from 'react'
+import ReactDOM from 'react-dom'
+// TO test anything that renders to the DOM
+import {getQueriesForElement, fireEvent} from '@testing-library/dom'
 
-// function Counter() {
-//   const [count, setCount] = React.useState(0)
-//   const increment = () => setCount(c => c + 1)
-//   return (
-//     <div>
-//       <button onClick={increment}>{count}</button>
-//     </div>
-//   )
-// }
+function Counter() {
+  const [count, setCount] = React.useState(0)
+  const increment = () => setCount(count => count + 1)
 
-function render(ui) {
-  // TODO
+  return (
+    <div>
+      <button onClick={increment}>{count}</button>
+    </div>
+  )
 }
 
-test('renders a counter', () => {
-  // TODO
+function render(ui) {
+  const container = document.createElement('div')
+  /**
+   * @see https://ja.reactjs.org/docs/react-dom.html#render
+   * 渡された container の DOM に React 要素をレンダーし、コンポーネントへの参照を返します。
+   */
+  ReactDOM.render(ui, container)
+  document.body.appendChild(container)
+  return {
+    ...getQueriesForElement(container),
+    container,
+    cleanup() {
+      ReactDOM.unmountComponentAtNode(container)
+      document.body.removeChild(container)
+    },
+  }
+}
+
+test('renders a container', () => {
+  const {getByText, cleanup} = render(<Counter />)
+  const counter = getByText('0')
+  fireEvent.click(counter)
+  expect(counter).toHaveTextContent('1')
+
+  fireEvent.click(counter)
+  expect(counter).toHaveTextContent('2')
+  cleanup()
 })
